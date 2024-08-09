7cf8e756a827ca8dc54f06174a76ae8f76dd2fd6
alanlima0 <88351930+alanlima0@users.noreply.github.com>
2024-08-09 19:28:45 -0300

    Update codigo2.c

--- codigo2.c
+++ codigo2.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*programa que possui um array que armazena 6 números inteiros e executa os seguintes passos:
+    1- Atribue os seguintes valores ao array: 1,0,5,-2,-5,7
+    2- Armazena em uma variavel chamada soma a soma dos valores das posições nums[0], nums [1], nums[5]
+    3 - Modifica o array nums na posição [4] atribuindo o valor 100.
+    4 - Mostra na tela a variavel soma e a cada valor do array nums, um em cada linha
+*/
 int main(){
     int nums[6]={1,0,5,-2,-5,7};
     int soma = nums[0] + nums[1] + nums[5];
